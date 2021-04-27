1f17bf46956648455cf38dbeae51c5b770c36c53
Denner <denner.vidal@gmail.com>
2021-04-27 13:38:54 -0400

    chore: adding theme definitions

--- packages/react-core/src/components/styles/definitions.ts
+++ packages/react-core/src/components/styles/definitions.ts
@@ -19,3 +19,105 @@ export const lightTheme = {
     contrast: '#fff',
   },
 };
+
+const misc = {
+  shadow: 'rgba(12, 15, 20, 0.07)',
+  overlay: 'rgba(12, 15, 20, 0.21)',
+  bodyBg: '#FAFBFC',
+  bodyColor: '#000',
+};
+
+const spacings = {
+  nano: '2px',
+  micro: '4px',
+  mili: '8px',
+  centi: '16px',
+  deca: '24px',
+  kilo: '32px',
+  mega: '40px',
+  giga: '48px',
+  tera: '56px',
+  peta: '64px',
+};
+
+const iconSizes = {
+  centi: '1rem', // 16px
+  deca: '1.5rem', // 24px
+  kilo: '2rem', // 32px
+};
+
+const borderRadius = {
+  nano: '2px',
+  micro: '4px',
+  mili: '8px',
+  circle: '100%',
+  pill: '999999px',
+};
+
+const borderWidth = {
+  pico: '1px',
+  nano: '2px',
+};
+
+const typography = {
+  headings: {
+    h5: {
+      fontSize: '1rem', // 16px
+      lineHeight: '1.5rem', // 24px
+    },
+    h4: {
+      fontSize: '1.125rem', // 18px
+      lineHeight: '1.688rem', // 27px
+    },
+    h3: {
+      fontSize: '1.25rem', // 20px
+      lineHeight: '2rem', // 32px
+    },
+    h2: {
+      fontSize: '1.625rem', // 26px
+      lineHeight: '2.375rem', // 38px
+    },
+    h1: {
+      fontSize: '2rem', // 32px
+      lineHeight: '2.625rem', // 42px
+    },
+  },
+  text: {
+    base: {
+      fontSize: '0.875rem', // 14px
+      lineHeight: '1.313rem', // 21px
+    },
+    sub: {
+      fontSize: '0.75rem', // 12px
+      lineHeight: '1.125rem', // 18px
+    },
+  },
+};
+
+const fontStack = {
+  default:
+    'Lato, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+  mono: 'Consolas, monaco, monospace',
+};
+
+const fontWeight = {
+  regular: '400',
+  bold: '700',
+  black: '900',
+};
+
+const fontStyles = {
+  normal: 'normal',
+  italic: 'italic',
+};
+
+/*
+
+2rem/32px	A Visual Type Scale - h1
+1.625rem/26px	A Visual Type Scale - h2
+1.25rem/20px	A Visual Type Scale - h3
+1.125rem/18px	A Visual Type Scale - h4
+1rem/16px	A Visual Type Scale - h5
+0.875rem/14.00px	A Visual Type Scale - text
+0.75rem/12px	A Visual Type Scale - subtitle
+ */
