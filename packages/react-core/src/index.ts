659f8b209709918685f8e051914ba58ae056901e
ryancarloscorrea <ryancarlos38@gmail.com>
2021-04-30 16:17:28 -0400

    chore: add exports components in index.js

--- packages/react-core/src/index.ts
+++ packages/react-core/src/index.ts
@@ -2,6 +2,15 @@ export { default as Button } from './components/atoms/button/Button';
 export * from './components/atoms/button/Button';
 export { default as Text } from './components/atoms/text/Text';
 export * from './components/atoms/text/Text';
-export { default as ThemeProvider } from './components/styles/ThemeProvider';
+export {
+  default as ThemeProvider,
+  ThemeProp,
+} from './components/styles/ThemeProvider';
 export { default as Input, InputProps } from './components/atoms/input/Input';
 export * from './components/styles/definitions';
+export { Paper } from './components/atoms/paper/Paper';
+export { Card } from './components/atoms/card/Card';
+export { Badge } from './components/atoms/badge/Badge';
+export { Switch } from './components/atoms/switch/Switch';
+export { RadioButton } from './components/atoms/radioButton/RadioButton';
+export { CheckBox } from './components/atoms/checkbox/CheckBox';
