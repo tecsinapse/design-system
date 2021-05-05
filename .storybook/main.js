2713d22768bdec8b4286862a55188700f0510b82
ryancarloscorrea <ryancarlos38@gmail.com>
2021-05-05 15:46:57 -0400

    feat: add docs mdx in storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,13 @@
 module.exports = {
-  stories: ['../packages/**/*.stories.@(tsx|jsx)'],
+  stories: [
+    '../packages/**/*.stories.@(tsx|jsx)',
+    '../docs/*/*.stories.@(js|ts|tsx|mdx)',
+  ],
   addons: [
     '@storybook/addon-storysource',
     '@storybook/addon-controls',
     '@storybook/addon-actions',
     '@storybook/addon-viewport',
+    '@storybook/addon-docs/',
   ],
 };
