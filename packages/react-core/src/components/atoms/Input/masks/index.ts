8d23621b3593226f6452f3a9cd0fbae4b4400da1
agnaldo <agnaldotb@gmail.com>
2021-05-28 10:49:50 -0300

    Ooops!

--- packages/react-core/src/components/atoms/Input/masks/index.ts
+++ packages/react-core/src/components/atoms/Input/masks/index.ts
@@ -3,7 +3,7 @@ export const CPF = '999.999.999-99';
 export const CNPJ = '99.999.999/9999-99';
 export const DATE = '99/99/9999';
 export const MONTH_YEAR = '99/9999';
-export const CEP = '88888-888';
+export const CEP = '99999-999';
 export const PHONE = '(99) 9999-9999';
 export const PHONE_EXTENDED = '(99) 99999-9999';
 export const COMBINED_PHONE = (value: string) => value.length <= 10 ? PHONE : PHONE_EXTENDED
