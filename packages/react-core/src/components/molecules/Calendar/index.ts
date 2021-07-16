910e704ce328882bd6e39acfd03fd1394ebae200
Hélio <helio_hfs@hotmail.com>
2021-07-16 12:43:47 -0400

    style: prettify

--- packages/react-core/src/components/molecules/Calendar/index.ts
+++ packages/react-core/src/components/molecules/Calendar/index.ts
@@ -1,1 +1,6 @@
-export { default as Calendar, CalendarProps, SelectionType, DateRange } from './Calendar';
+export {
+  default as Calendar,
+  CalendarProps,
+  SelectionType,
+  DateRange,
+} from './Calendar';
