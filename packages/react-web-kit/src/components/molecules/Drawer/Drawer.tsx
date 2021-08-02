ef0279912491da3014186817206b5557e37d3b80
ryancarloscorrea <ryancarlos38@gmail.com>
2021-08-02 15:08:16 -0400

    feat: add animation opacity in drawer

--- packages/react-web-kit/src/components/molecules/Drawer/Drawer.tsx
+++ packages/react-web-kit/src/components/molecules/Drawer/Drawer.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 import { StyledContainerDrawer, StyledOverlay } from './styled';
+import { Transition } from 'react-transition-group';
 
 export interface DrawerProps {
   open: boolean;
@@ -13,20 +14,62 @@ const Drawer: FC<DrawerProps> = ({
   onClose,
   children,
 }) => {
+  const defaultStyleDrawer = {
+    transition: `width 1000ms`,
+    width: 0,
+    height: 0,
+  };
+
+  const defaultStyle = {
+    transition: `opacity 1000ms ease-in-out`,
+    opacity: 0,
+  };
+
+  const transitionStylesDrawer = {
+    entering: { width: 0 },
+    entered: { width: '100%' },
+    exiting: { width: '100%' },
+    exited: { width: 0 },
+  };
+  const transitionStyles = {
+    entering: { opacity: 1 },
+    entered: { opacity: 1 },
+    exiting: { opacity: 0 },
+    exited: { opacity: 0 },
+  };
+
   return (
     <>
-      {open && (
-        <>
-          <StyledOverlay onClick={onClose} />
-          <StyledContainerDrawer
-            anchorPosition={anchorPosition}
-            onClose={onClose}
-            open={open}
+      <Transition in={open} timeout={10000}>
+        {state => (
+          <div
+            style={{
+              ...defaultStyle,
+              ...transitionStyles[state],
+            }}
           >
-            {children}
-          </StyledContainerDrawer>
-        </>
-      )}
+            <StyledOverlay onClick={onClose} />
+          </div>
+        )}
+      </Transition>
+      <Transition in={open} timeout={10000}>
+        {state => (
+          <div
+            style={{
+              ...defaultStyle,
+              ...transitionStyles[state],
+            }}
+          >
+            <StyledContainerDrawer
+              anchorPosition={anchorPosition}
+              onClose={onClose}
+              open={open}
+            >
+              {children}
+            </StyledContainerDrawer>
+          </div>
+        )}
+      </Transition>
     </>
   );
 };
