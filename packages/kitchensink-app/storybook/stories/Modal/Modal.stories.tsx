97da58d5a4b233d50b2612145e36bcbc15ee8e40
agnaldo <agnaldotb@gmail.com>
2021-12-27 14:20:57 -0300

    Added a modal story.

--- packages/kitchensink-app/storybook/stories/Modal/Modal.stories.tsx
+++ packages/kitchensink-app/storybook/stories/Modal/Modal.stories.tsx
@@ -1,7 +1,8 @@
 import { storiesOf } from '@storybook/react-native';
-import { Button, ModalManager, ModalView, Text, useModalManager } from '@tecsinapse/react-native-kit';
-import { IBaseModal } from 'packages/react-native-kit/src/components/atoms/Modal/ui/types';
-import React, { FC, useState } from 'react';
+import { Button, IBaseModal, ModalGroupManager, ModalView, Text, useModalManager } from '@tecsinapse/react-native-kit';
+import React, { FC } from 'react';
+import { View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { ArtBoard } from '../ArtBoard';
 
 storiesOf('Modal', module)
@@ -10,104 +11,30 @@ storiesOf('Modal', module)
     return <Component />;
   });
 
-
-
-// const Cp1 = ({ action }) => {
-//   const [text, setText] = useState('First text!')
-//   useEffect(() => {
-//     console.log("run animation!");
-//     return () => console.log("destroyed!");
-//   }, [])
-//   return (
-//     <View style={{ backgroundColor: 'red' }}>
-//       <Text typography='h2'>{text}</Text>
-//       <Button color='info' onPress={() => {
-//         setText("wow!")
-//         action?.()
-//       }}><Text>Modal fire!</Text></Button>
-//       <Button color='info' onPress={() => {} }><Text>Close current modal</Text></Button>
-//     </View>
-//   )
-// }
-
-
-
-
-interface Opa extends IBaseModal { 
-  pass?: number
-}
-
-const Cp5: FC<Opa> = ({ ...others }) => {
-  return (
-    <ModalView {...others }>
-      <Text typography='h2'>Modal 5</Text>
-    </ModalView>
-  )
-}
-
-const Cp4: FC<Opa> = ({ close, ...others }) => {
+const MyModal: FC<IBaseModal> = ({ close, ...others }) => {
   return (
     <ModalView {...others } close={close}>
-      <Text typography='h2'>Modal 4</Text>
-      <Button onPress={close}><Text>Close modal 4</Text></Button>
+      <View style={{ padding: 20 }}>
+        <Text typography='h2'>Hey, I'm a modal!</Text>
+        <Button onPress={close}><Text>Close me!</Text></Button>
+      </View>
     </ModalView>
   )
 }
-
-const Cp3: FC<Opa> = ({ pass, close, ...others }) => {
-  const opa = useModalManager('modal4', () => <Cp4/>)
-  return (
-    <ModalView {...others } close={close}>
-      <Text typography='h2'>Modal 3 ({pass})</Text>
-      <Button onPress={() => opa.show()}><Text>Open modal 4</Text></Button>
-      <Button onPress={close}><Text>Close current modal</Text></Button>
-    </ModalView>
-  )
-}
-
-const Cp2: FC<Opa> = ({ close, ...others }) => {
-  const [ num, setNum ] = useState(0)
-  const opa = useModalManager('modal3', () => <Cp3 pass={num}/>)
-  // console.log("MODAL->", others)
-  return (
-    <ModalView {...others } close={close}>
-      <Text typography='h2'>Modal 2 ({num})</Text>
-      <Button onPress={() => opa.show()}><Text>Open modal 3</Text></Button>
-      <Button onPress={close}><Text>Close current modal</Text></Button>
-      <Button onPress={() => setNum(old => old + 1)}><Text>+</Text></Button>
-    </ModalView>
-  )
-}
-
 
 const App = () => {
-  const ha = () => console.log("Fire!!");
-  const md1 = useModalManager('modal2', () => <Cp2/>)
-  const md5 = useModalManager('modal5', () => <Cp5/>)
+  const myModal = useModalManager(() => <MyModal/>)
   return (
-    <>
-      {/* <Cp1/> */}
-      <Button onPress={() => md1.show() }><Text>Open modal</Text></Button>
-      <Button onPress={() => md5.show() }><Text>Open modal5</Text></Button>
-      <Button onPress={() => md1.close() }><Text>Close modal</Text></Button>
-    </>
+    <Button onPress={() => myModal.show() }><Text>Open a little modal</Text></Button>
   )
 }
 
-
-
-
-
-
-
 const Component = () => {
-  
   return (
-    <>
-      {/* Manager */}
-      <ModalManager>
+    <SafeAreaProvider>
+      <ModalGroupManager>
         <App/>
-      </ModalManager>
-    </>
+      </ModalGroupManager>
+    </SafeAreaProvider>
   );
 };
