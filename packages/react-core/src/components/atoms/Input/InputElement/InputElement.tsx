f556abce99def79689a8d46a263b1fafea2b920a
lucaspr98 <lucaspr98@gmail.com>
2022-02-14 10:38:13 -0400

    chore: adding documentation

--- packages/react-core/src/components/atoms/Input/InputElement/InputElement.tsx
+++ packages/react-core/src/components/atoms/Input/InputElement/InputElement.tsx
@@ -13,9 +13,14 @@ export interface InputElementProps
   placeholder?: string;
   disabled?: boolean;
   onChange?: (value: any) => void;
-  /*
-
-   */
+  /**
+   To use mask for strings you have to pass a MaskType[] or ((value: string) => MaskType[])
+   A MaskType can be a string, RegExp, or Array<RegExp>.
+   In case we have a string, '9' represents digits, 'a' represents alphabet characters, and any other character represents fixed characters in the mask.
+   For example a phone mask can be represented as ['(99) \\99999-9999'], or ['(99) ', '/[9]/', '9999-9999'], or ['(', /[0-9]/, /[0-9]/, ') ', '/[9]/', '9999-9999'] and many others.
+   To use mask for numbers you have to pass a CurrencyOptions object
+   A CurrencyOptions object contains symbol, separator, decimal, precision. For example {symbol: 'R$ ', separator: '.', decimal: ',', precision: 2,}.
+   **/
   mask?: (MaskType[] | ((value: string) => MaskType[])) | CurrencyOptions;
   onFocus?: () => void;
   onBlur?: () => void;
