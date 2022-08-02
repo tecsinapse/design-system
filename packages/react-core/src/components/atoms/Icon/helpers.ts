f42a7340e668b5cfba6a82d133a88a7669f0fe52
Denner <denner.vidal@gmail.com>
2022-08-02 16:12:53 -0400

    fix: require vector-icons direct from /dist

--- packages/react-core/src/components/atoms/Icon/helpers.ts
+++ packages/react-core/src/components/atoms/Icon/helpers.ts
@@ -24,73 +24,74 @@ export const getIconComponent = (type: IconType, size: IconSizeType): any => {
   switch (type) {
     case 'zocial':
       return getStyledIcon(
-        require('react-native-vector-icons/Zocial').default,
+        require('react-native-vector-icons/dist/Zocial').default,
         size
       );
     case 'octicon':
       return getStyledIcon(
-        require('react-native-vector-icons/Octicons').default,
+        require('react-native-vector-icons/dist/Octicons').default,
         size
       );
     case 'material':
       return getStyledIcon(
-        require('react-native-vector-icons/MaterialIcons').default,
+        require('react-native-vector-icons/dist/MaterialIcons').default,
         size
       );
     case 'material-community':
       return getStyledIcon(
-        require('react-native-vector-icons/MaterialCommunityIcons').default,
+        require('react-native-vector-icons/dist/MaterialCommunityIcons')
+          .default,
         size
       );
     case 'ionicon':
       return getStyledIcon(
-        require('react-native-vector-icons/Ionicons').default,
+        require('react-native-vector-icons/dist/Ionicons').default,
         size
       );
     case 'foundation':
       return getStyledIcon(
-        require('react-native-vector-icons/Foundation').default,
+        require('react-native-vector-icons/dist/Foundation').default,
         size
       );
     case 'evilicon':
       return getStyledIcon(
-        require('react-native-vector-icons/EvilIcons').default,
+        require('react-native-vector-icons/dist/EvilIcons').default,
         size
       );
     case 'entypo':
       return getStyledIcon(
-        require('react-native-vector-icons/Entypo').default,
+        require('react-native-vector-icons/dist/Entypo').default,
         size
       );
     case 'font-awesome':
       return getStyledIcon(
-        require('react-native-vector-icons/FontAwesome').default,
+        require('react-native-vector-icons/dist/FontAwesome').default,
         size
       );
     case 'font-awesome-5':
       return getStyledIcon(
-        require('react-native-vector-icons/FontAwesome5').default,
+        require('react-native-vector-icons/dist/FontAwesome5').default,
         size
       );
     case 'simple-line-icon':
       return getStyledIcon(
-        require('react-native-vector-icons/SimpleLineIcons').default,
+        require('react-native-vector-icons/dist/SimpleLineIcons').default,
         size
       );
     case 'feather':
       return getStyledIcon(
-        require('react-native-vector-icons/Feather').default,
+        require('react-native-vector-icons/dist/Feather').default,
         size
       );
     case 'antdesign':
     case 'ant-design':
       return getStyledIcon(
-        require('react-native-vector-icons/AntDesign').default,
+        require('react-native-vector-icons/dist/AntDesign').default,
         size
       );
     case 'fontisto':
       return getStyledIcon(
-        require('react-native-vector-icons/Fontisto').default,
+        require('react-native-vector-icons/dist/Fontisto').default,
         size
       );
     default:
@@ -98,7 +99,7 @@ export const getIconComponent = (type: IconType, size: IconSizeType): any => {
         return getStyledIcon(customIcons[type], size);
       }
       return getStyledIcon(
-        require('react-native-vector-icons/MaterialIcons').default,
+        require('react-native-vector-icons/dist/MaterialIcons').default,
         size
       );
   }
