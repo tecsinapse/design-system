c1ec39bd9c9bd57649e56fdcdb7026d3a73928d2
Denner <denner.vidal@gmail.com>
2022-09-02 15:08:13 -0400

    fix: cannot read values from emotion

--- packages/react-web-kit/src/components/molecules/Grid/Item/functions.ts
+++ packages/react-web-kit/src/components/molecules/Grid/Item/functions.ts
@@ -6,8 +6,8 @@ export const getSpan = (
   md: boolean,
   lg: boolean
 ): number => {
-  if (sm) return value.sm;
-  if (md) return value.md ?? value.sm;
-  if (lg) return value.lg ?? value.md ?? value.sm;
+  if (sm) return value?.sm;
+  if (md) return value?.md ?? value?.sm;
+  if (lg) return value?.lg ?? value?.md ?? value?.sm;
   return 0;
 };
