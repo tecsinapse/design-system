c6a747426c28a7d06212877ef4eb37fe4723b430
Denner <denner.vidal@gmail.com>
2022-09-19 09:14:31 -0400

    feat: try rollup + esbuild

--- packages/react-core/rollup.config.js
+++ packages/react-core/rollup.config.js
@@ -1,8 +1,4 @@
-import commonjs from '@rollup/plugin-commonjs';
-import json from '@rollup/plugin-json';
-import resolve from '@rollup/plugin-node-resolve';
-import typescript from '@rollup/plugin-typescript';
-import { terser } from 'rollup-plugin-terser';
+import esbuild from 'rollup-plugin-esbuild';
 
 const outputDefaultConfig = {
   preserveModules: true,
@@ -15,28 +11,16 @@ export default [
     input: ['./src/index.ts'],
     output: [
       {
-        dir: 'rollup/',
+        dir: 'rollup/esm',
         format: 'esm',
         ...outputDefaultConfig,
       },
       {
-        dir: 'rollup/',
+        dir: 'rollup/cjs',
         format: 'cjs',
-        exports: 'auto',
         ...outputDefaultConfig,
       },
     ],
-    plugins: [
-      resolve(),
-      commonjs(),
-      typescript({
-        tsconfig: './tsconfig.build.json',
-        declaration: true,
-        declarationDir: 'rollup/',
-      }),
-      terser(),
-      json(),
-    ],
-    external: ['react', 'react-dom', 'react-native', 'react-vector-icons'],
+    plugins: [esbuild()],
   },
 ];
