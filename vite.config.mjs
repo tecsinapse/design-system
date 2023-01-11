4becd6e30eee1323316e016a0c178e36fbe5c84a
Denner <denner.vidal@gmail.com>
2023-01-11 15:26:23 -0400

    feat: upgrade storybook web

---
+++ vite.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react-swc'
+
+export default defineConfig(({ mode }) => ({
+  plugins: [react()],
+  resolve: { alias: { 'react-native': 'react-native-web' } }
+}))
