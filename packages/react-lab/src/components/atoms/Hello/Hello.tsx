186c455401675607b8f6cbdd213c5d71016a8587
beatriz.silva <beatriz.silva@tecsinapse.com.br>
2023-04-18 15:21:10 -0300

    feat drag list

--- packages/react-lab/src/components/atoms/Hello/Hello.tsx
+++ packages/react-lab/src/components/atoms/Hello/Hello.tsx
@@ -1,6 +1,102 @@
-import React from 'react';
-import { DivStyled } from './styled';
+import React, { createElement, useRef, useState } from 'react';
+import { DivStyledContainer, ListContainer, ListContainerTo } from './styled';
 
-const Hello = () => <DivStyled>Hello</DivStyled>;
+const Hello = () => {
+  const dragRef = useRef(null);
+  const dropRef = useRef(null);
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleDragStart = e => {
+    e.dataTransfer.setData('id', e.target.id);
+  };
+  const handleDragOver = e => {
+    e.preventDefault();
+  };
+
+  const handleDrop = e => {
+    e.preventDefault();
+    const draggedId = e.dataTransfer.getData('id');
+    const draggedElement = document.getElementById(draggedId);
+    const dropElement = dropRef?.current;
+    dropElement.append(draggedElement);
+  };
+
+  const handleListClick = e => {
+    const nodeItem = dropRef?.current;
+  };
+
+  const items = [
+    {
+      id: 1,
+      name: 'Ana Rita',
+      type: 'toDo',
+    },
+    {
+      id: 2,
+      name: 'Caio',
+      type: 'toDo',
+    },
+    {
+      id: 3,
+      name: 'Jorge',
+      type: 'toDo',
+    },
+    {
+      id: 4,
+      name: 'Beatriz',
+      type: 'done',
+    },
+    {
+      id: 5,
+      name: 'Carla',
+      type: 'done',
+    },
+    {
+      id: 6,
+      name: 'Ana',
+      type: 'done',
+    },
+  ];
+
+  const currentStyle = {
+    backgroundColor: isClicked ? 'white' : 'white',
+    cursor: 'grab',
+  };
+
+  return (
+    <DivStyledContainer>
+      <ListContainer>
+        <ul style={{ width: '100%', height: '100%', display: 'inline-block' }}>
+          {items.map(item => (
+            <li
+              key={item.id}
+              style={currentStyle}
+              ref={dragRef}
+              id={item.id.toString()}
+              draggable
+              onDragStart={handleDragStart}
+              onClick={handleListClick}
+            >
+              {' '}
+              {item.name}
+            </li>
+          ))}
+        </ul>
+      </ListContainer>
+      <ListContainer>
+        {' '}
+        <ul
+          id={'drop'}
+          ref={dropRef}
+          onDragOver={handleDragOver}
+          onDrop={handleDrop}
+          style={{ width: '100%', height: '100%', display: 'inline-block' }}
+        >
+          {' '}
+        </ul>
+      </ListContainer>
+    </DivStyledContainer>
+  );
+};
 
 export default Hello;
--- packages/react-lab/src/components/atoms/Hello/styled.ts
+++ packages/react-lab/src/components/atoms/Hello/styled.ts
@@ -1,7 +1,38 @@
 import styled from '@emotion/styled';
 
-export const DivStyled = styled('div')`
+export const DivStyledContainer = styled('div')`
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
   display: flex;
+  flex-direction: row;
+  width: 400px;
+`;
+
+export const ListContainer = styled('div')`
+  align-items: center;
+  width: 400px;
+  display: flex;
+  justify-content: center;
+  height: 200px;
+  flex-direction: column;
+  border-style: solid;
+  border-color: black;
+  border-width: 1px;
+  overflow-y: scroll;
+`;
+
+export const ListContainerTo = styled('ul')`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  list-style-type: none;
+`;
+
+export const ListContainerFinal = styled('ul')`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  list-style-type: none;
 `;
