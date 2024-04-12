07400000d7ff0f2abec6ed769356b0c5cc497a4e
ryancarloscorrea <ryancarlos38@gmail.com>
2024-04-12 12:19:42 -0400

    [TS-5709] ajustes de interface do cortex

--- packages/cortex-core/src/components/modal/modal.ts
+++ packages/cortex-core/src/components/modal/modal.ts
@@ -1,5 +1,11 @@
 import { tv } from 'tailwind-variants';
 
 export const modal = tv({
-  base: 'inset-x-0 fixed mx-auto rounded-micro p-kilo bg-white shadow-default flex transition fixed invisible peer-checked:visible scale-0 peer-checked:scale-100',
+  base: 'fixed rounded-micro p-kilo bg-white shadow-default flex transition inset-0',
+  variants: {
+    open: {
+      true: 'scale-100 visible',
+      false: 'invisible',
+    },
+  },
 });
