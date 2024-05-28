88e355bc726a0f3dfe0a5cb2a3471a624c21fa38
Bruno Aires <brunoferreiraaires@gmail.com>
2024-05-28 10:54:27 -0300

    [#TS-6559] [QA] Pesquisar no menu inicial não retorna as opções

--- packages/react-web-kit/src/components/molecules/Menubar/SearchResultItem/utils.tsx
+++ packages/react-web-kit/src/components/molecules/Menubar/SearchResultItem/utils.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const normalize = (str: string): string => {
+export const normalize = (str: string): string => {
   return str
     .normalize('NFD')
     .replace(/[\u0300-\u036f]/g, '')
