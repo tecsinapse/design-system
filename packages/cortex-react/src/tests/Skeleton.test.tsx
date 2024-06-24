cb7ce772b6a00401b5184fefefb7215e2fd44a4b
ryancarloscorrea <ryancarlos38@gmail.com>
2024-06-24 15:32:34 -0400

    [S-6973] feat: Adicionando testes unitários para o Skeleton

---
+++ packages/cortex-react/src/tests/Skeleton.test.tsx
@@ -0,0 +1,13 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { Skeleton } from '../components';
+
+describe('Skeleton', () => {
+  it('Should render Skeleton', () => {
+    render(<Skeleton />);
+    const skeleton = screen.getByTestId('skeleton');
+    expect(skeleton).toBeInTheDocument();
+    expect(skeleton).toHaveClass('animate-pulse bg-secondary-light');
+  });
+});
