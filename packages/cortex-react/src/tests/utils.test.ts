466806287b9a724f4d7515895170963a2580a02a
ryancarloscorrea <ryancarlos38@gmail.com>
2024-06-25 09:48:53 -0400

    [TS-6973] feat: Adicionando arquivo useDebouncedState

--- packages/cortex-react/src/tests/utils.test.ts
+++ packages/cortex-react/src/tests/utils.test.ts
@@ -1,7 +1,5 @@
 import '@testing-library/jest-dom';
 import { getNameInitials } from '../components/utils';
-import { renderHook, act } from '@testing-library/react';
-import { useDebouncedState } from '../hooks';
 
 describe('GetNameInitials', () => {
   it('test getNameInitials single name', () => {
@@ -20,59 +18,5 @@ describe('GetNameInitials', () => {
 
   it('test getNameInitials empty name', () => {
     expect(getNameInitials('')).toBe(undefined);
-  });
-});
-
-describe('useDeboundedState', () => {
-  beforeEach(() => {
-    jest.useFakeTimers();
-  });
-
-  afterEach(() => jest.useRealTimers());
-
-  it('Should initialize with the initial state', () => {
-    const { result } = renderHook(() => useDebouncedState('initial'));
-
-    const [state] = result.current;
-    expect(state).toBe('initial');
-  });
-
-  it('Should update state immediately', () => {
-    const timeoutCallback = jest.fn();
-
-    const { result } = renderHook(() =>
-      useDebouncedState<string>('initial', timeoutCallback, 0)
-    );
-
-    const [, setState] = result.current;
-
-    act(() => {
-      setState('updated');
-    });
-
-    const [state] = result.current;
-
-    expect(state).toBe('updated');
-  });
-
-  it('Should call timeoutCallback after timeout', () => {
-    const timeoutCallback = jest.fn();
-    const { result } = renderHook(() =>
-      useDebouncedState<string>('initial', timeoutCallback, 500)
-    );
-
-    const [, setState] = result.current;
-
-    act(() => {
-      setState('updated');
-    });
-
-    expect(timeoutCallback).not.toHaveBeenCalled();
-
-    act(() => {
-      jest.advanceTimersByTime(500);
-    });
-
-    expect(timeoutCallback).toHaveBeenCalledWith('updated');
   });
 });
