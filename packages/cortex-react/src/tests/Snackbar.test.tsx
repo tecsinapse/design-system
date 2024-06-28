04b9725442abcc68f38202a022e002f864f441d8
ryancarloscorrea <ryancarlos38@gmail.com>
2024-06-28 09:09:16 -0400

    [TS-6973] Ajustes PR

--- packages/cortex-react/src/tests/Snackbar.test.tsx
+++ packages/cortex-react/src/tests/Snackbar.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { BaseSnackbar, DefaultSnack } from '../components';
+import { BaseSnackbar } from '../components';
 import { render, screen } from '@testing-library/react';
 
 describe('BaseSnackBar', () => {
@@ -88,26 +88,5 @@ describe('BaseSnackBar', () => {
     const snack = screen.getByTestId('snackbar');
     expect(snack).toHaveClass('bg-secondary-xlight');
     expect(snack).toHaveClass('text-secondary-medium');
-  });
-});
-
-describe('DefaultSnack', () => {
-  it('Should render DefaultSnack', () => {
-    const textSnack = 'Text example';
-    render(<DefaultSnack text={textSnack} onDismiss={jest.fn} />);
-    const snack = screen.getByTestId('snackbar');
-    const iconInfo = screen.getByTestId('icon-info');
-    const iconClose = screen.getByTestId('icon-close');
-
-    expect(snack).toBeInTheDocument();
-    expect(iconInfo).toBeInTheDocument();
-    expect(iconClose).toBeInTheDocument();
-  });
-  it('Should render correctly text', () => {
-    const textSnack = 'Text example';
-    render(<DefaultSnack text={textSnack} onDismiss={jest.fn} />);
-    const snack = screen.getByTestId('snackbar');
-
-    expect(snack).toHaveTextContent(textSnack);
   });
 });
