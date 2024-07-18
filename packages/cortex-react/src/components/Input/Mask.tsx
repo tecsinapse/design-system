27f4812081291b8460157a4487188b137a68fb0a
lucaspr98 <lucas.ramos@tecsinapse.com.br>
2024-07-18 16:20:07 -0400

    chore(TS-7555): forwardRef + common hook

--- packages/cortex-react/src/components/Input/Mask.tsx
+++ packages/cortex-react/src/components/Input/Mask.tsx
@@ -1,73 +1,63 @@
-import React, { Ref, useEffect } from 'react';
+import React, { MutableRefObject, Ref, createRef, useEffect } from 'react';
 import { useIMask } from 'react-imask';
 import { Input } from '.';
 import { InputMaskProps, InputProps } from './types';
 
+const useIMaskLocal = (
+  mask: any,
+  inputProps: InputProps,
+  ref?: React.ForwardedRef<HTMLInputElement>
+) => {
+  const { ref: iMaskRef, maskRef } = useIMask(mask, {
+    onAccept: () => inputProps.onChange?.(maskRef.current),
+    ref: ref ? (ref as MutableRefObject<HTMLInputElement>) : createRef(),
+  });
+
+  useEffect(() => {
+    if (inputProps.value === '') {
+      maskRef.current?.updateValue();
+    }
+  }, [maskRef, inputProps.value]);
+
+  return {
+    iMaskRef,
+  };
+};
+
 export const InputMaskExpression = React.forwardRef<
   HTMLInputElement,
   InputMaskProps
->(({ mask, ...rest }) => {
-  const { ref: iMaskRef, maskRef } = useIMask(
-    { mask },
-    {
-      onAccept: () => rest?.onChange?.(maskRef.current),
-    }
-  );
-
-  useEffect(() => {
-    if (rest.value === '') {
-      maskRef.current?.updateValue();
-    }
-  }, [maskRef, rest.value]);
+>(({ mask, ...rest }, ref) => {
+  const { iMaskRef } = useIMaskLocal({ mask }, rest, ref);
 
   return <Input.Root {...rest} ref={iMaskRef as Ref<HTMLInputElement>} />;
 });
 
 export const InputMaskNumber = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ ...rest }) => {
-    const { ref: iMaskRef, maskRef } = useIMask(
-      { mask: Number, scale: 2 },
-      {
-        onAccept: () => rest?.onChange?.(maskRef.current),
-      }
-    );
-
-    useEffect(() => {
-      if (rest.value === '') {
-        maskRef.current?.updateValue();
-      }
-    }, [maskRef, rest.value]);
+  ({ ...rest }, ref) => {
+    const mask = { mask: Number, scale: 2 };
+    const { iMaskRef } = useIMaskLocal(mask, rest, ref);
 
     return <Input.Root {...rest} ref={iMaskRef as Ref<HTMLInputElement>} />;
   }
 );
 
 export const InputMaskCurrency = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ ...rest }) => {
-    const { ref: iMaskRef, maskRef } = useIMask(
-      {
-        mask: 'R$ num',
-        blocks: {
-          num: {
-            mask: Number,
-            scale: 2,
-            thousandsSeparator: '.',
-            padFractionalZeros: true,
-            radix: ',',
-            mapToRadix: ['.'],
-          },
+  ({ ...rest }, ref) => {
+    const mask = {
+      mask: 'R$ num',
+      blocks: {
+        num: {
+          mask: Number,
+          scale: 2,
+          thousandsSeparator: '.',
+          padFractionalZeros: true,
+          radix: ',',
+          mapToRadix: ['.'],
         },
       },
-      {
-        onAccept: () => rest?.onChange?.(maskRef.current),
-      }
-    );
-
-    useEffect(() => {
-      if (rest.value === '') {
-        maskRef.current?.updateValue();
-      }
-    }, [maskRef, rest.value]);
+    };
+    const { iMaskRef } = useIMaskLocal(mask, rest, ref);
 
     return <Input.Root {...rest} ref={iMaskRef as Ref<HTMLInputElement>} />;
   }
