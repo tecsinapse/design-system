30a36e808f16993f51dcb46b9a9d376ca0d41791
ryancarloscorrea <ryancarlos38@gmail.com>
2024-07-23 17:17:18 -0400

    test publish manual

--- packages/cortex-core/src/components/common/common.ts
+++ packages/cortex-core/src/components/common/common.ts
@@ -4,7 +4,7 @@ export const overlay = tv({
   base: 'fixed',
   variants: {
     show: {
-      true: 'bg-opacity-50 cursor-pointer z-[0] bg-black h-full w-full left-0 top-0 transition',
+      true: 'bg-opacity-50 z-[0] bg-black h-full w-full left-0 top-0 transition cursor-pointer',
       false: 'invisible',
     },
   },
