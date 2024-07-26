358955bfff2fafff54fee1ff3570e9a267306684
lucaspr98 <lucas.ramos@tecsinapse.com.br>
2024-07-26 14:42:57 -0400

    fix: DatePickerInputBase style

--- packages/cortex-react/src/components/DatePickerInputBase.tsx
+++ packages/cortex-react/src/components/DatePickerInputBase.tsx
@@ -17,7 +17,9 @@ export const DatePickerInputBase = ({
   return (
     <Input.Face variants={variants} data-testid={'date-picker-input-base'}>
       <span className={labelStyle({})}>{label}</span>
-      <div className={inputBox('', label)}>{children}</div>
+      <div className={inputBox('', label, 'w-full flex flex-row')}>
+        {children}
+      </div>
       <Input.Right className={''}>
         <LiaCalendar
           className="cursor-pointer mt-centi"
