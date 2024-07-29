791db4f6f3ef6cc790a2c056d0e2f024e0804371
ryancarloscorrea <ryancarlos38@gmail.com>
2024-07-29 11:41:11 -0400

    fix Drawer e export interface Button

--- packages/cortex-react/src/components/Button.tsx
+++ packages/cortex-react/src/components/Button.tsx
@@ -1,24 +1,24 @@
 import React, { forwardRef } from 'react';
 import { button, ButtonVariants } from '@tecsinapse/cortex-core';
 
-interface ButtonProps {
+export interface ButtonProps
+  extends Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'className'> {
   variants?: ButtonVariants;
   children?: React.ReactNode;
 }
 
-export const Button = forwardRef<
-  HTMLButtonElement,
-  ButtonProps & Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'className'>
->((props, ref) => {
-  const { variants, children, ...rest } = props;
-  return (
-    <button
-      className={button(variants)}
-      ref={ref}
-      data-testid="button"
-      {...rest}
-    >
-      {children}
-    </button>
-  );
-});
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  (props, ref) => {
+    const { variants, children, ...rest } = props;
+    return (
+      <button
+        className={button(variants)}
+        ref={ref}
+        data-testid="button"
+        {...rest}
+      >
+        {children}
+      </button>
+    );
+  }
+);
