af9f48ca6fc8ce6d640c1a203d343a36fbd72435
ryancarloscorrea <ryancarlos38@gmail.com>
2024-10-28 17:27:20 -0400

    [TS-9554] refactor: removendo useDimensions de Left e Search

--- packages/cortex-react/src/components/Menubar/Left.tsx
+++ packages/cortex-react/src/components/Menubar/Left.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import { DefaultProps } from './types';
 import { menubar } from '../../styles/menubar';
-import { useDimensions } from '../../hooks';
 
 const { left } = menubar();
 
 const Left = ({ children, ...rest }: DefaultProps) => {
-  const { width } = useDimensions();
-  if (width <= 640) return <></>;
   return (
     <div className={left()} data-testid={'header-left-menubar'} {...rest}>
       {children}
--- packages/cortex-react/src/components/Menubar/Search.tsx
+++ packages/cortex-react/src/components/Menubar/Search.tsx
@@ -3,21 +3,14 @@ import { Input, InputSearchProps } from '../Input';
 import clsx from 'clsx';
 import { animate } from '../../styles/menubar';
 import { useMenubar } from '../../provider';
-import { useDimensions } from '../../hooks';
 
 const Search = (props: InputSearchProps) => {
   const [show] = useMenubar();
-  const { width } = useDimensions();
 
   return (
     <Input.Search
       {...props}
-      className={clsx(
-        { 'mx-deca': width <= 640 },
-        { 'mr-deca': width > 640 },
-        'flex-1',
-        animate({ show: show })
-      )}
+      className={clsx('flex-1', animate({ show: show }))}
     />
   );
 };
