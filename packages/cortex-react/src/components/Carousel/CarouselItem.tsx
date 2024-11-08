c7ddb1f8c6918ccfe803ea84fde7e7673f166e60
ryancarloscorrea <ryancarlos38@gmail.com>
2024-11-08 11:30:01 -0400

    feat: responsividade no Carousel

--- packages/cortex-react/src/components/Carousel/CarouselItem.tsx
+++ packages/cortex-react/src/components/Carousel/CarouselItem.tsx
@@ -20,19 +20,22 @@ export const CarouselItem = ({ item }: CarouselItemProps) => {
   return (
     <>
       <img
-        className={'w-full h-[360px] object-fill rounded-mili'}
+        className={'w-full h-[360px] object-cover rounded-mili'}
         src={item.src}
         alt={item.alt}
       />
       <div
         className={
-          'z-absolute absolute top-[50%] transform -translate-y-[50%] left-[10%] max-w-[60%]'
+          'z-absolute absolute top-[50%] transform -translate-y-[50%] left-[10%] mx-deca max-w-[60%]'
         }
       >
         <p className={clsx('text-h2 font-bold')} style={stylesTitle}>
           {item.title.text}
         </p>
-        <p className={'text-white text-h2 font-bold'} style={stylesSubTitle}>
+        <p
+          className={'text-white text-h2 font-bold hidden lg:flex'}
+          style={stylesSubTitle}
+        >
           {item.subtitle.text}
         </p>
       </div>
