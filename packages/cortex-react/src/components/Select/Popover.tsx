49308833c9466c12a9ae93bbfcefd442ca3df27f
Gabriel Maciel <gabriellive97@hotmail.com>
2025-03-17 17:33:49 -0300

    fix: popover width select

--- packages/cortex-react/src/components/Select/Popover.tsx
+++ packages/cortex-react/src/components/Select/Popover.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import clsx from 'clsx';
 import { Popover } from '../Popover';
 import { SelectPopoverProps } from './types';
+import { FloatingPortal } from '@floating-ui/react';
+import { SelectContext } from './context';
 
 export const SelectPopover = ({ children }: SelectPopoverProps) => {
+  const { triggerWidth } = useContext(SelectContext);
+  const widthClass = triggerWidth ? `w-[${triggerWidth}px]` : 'w-auto';
+
   return (
-    <Popover.Content className="bg-white max-h-[30vh] w-full overflow-y-scroll gap-y-mili flex flex-col p-0">
-      {children}
-    </Popover.Content>
+    <FloatingPortal>
+      <Popover.Content
+        className={clsx(
+          'bg-white max-h-[30vh] overflow-y-scroll gap-y-mili flex flex-col p-0',
+          widthClass
+        )}
+      >
+        {children}
+      </Popover.Content>
+    </FloatingPortal>
   );
 };
