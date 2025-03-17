5b6aad02ae69c5b9026241a13da9ce4638fd09b8
Gabriel Maciel <gabriellive97@hotmail.com>
2025-03-16 22:28:49 -0300

    fix: removing popover portal

--- packages/cortex-react/src/components/Popover/Content.tsx
+++ packages/cortex-react/src/components/Popover/Content.tsx
@@ -37,26 +37,25 @@ export const PopoverContent = ({
   return (
     <>
       {isOpen && (
-        <FloatingPortal preserveTabOrder>
-          <FloatingFocusManager {...rest} context={context} modal={modal}>
-            <div
-              ref={refs.setFloating}
-              {...getFloatingProps()}
-              className={clsx(
-                'border border-gray-200 bg-black p-[0px] rounded-md shadow-lg z-popover',
-                className
-              )}
-              style={{
-                position: strategy,
-                top: y ?? 0,
-                left: x ?? 0,
-                ...floatingStyles,
-              }}
-            >
-              {children}
-            </div>
-          </FloatingFocusManager>
-        </FloatingPortal>
+        <FloatingFocusManager {...rest} context={context} modal={modal}>
+          <div
+            ref={refs.setFloating}
+            {...getFloatingProps()}
+            className={clsx(
+              'border border-gray-200 bg-black p-[0px] rounded-md shadow-lg z-popover',
+              className
+            )}
+            style={{
+              position: strategy,
+              top: y ?? 0,
+              left: x ?? 0,
+
+              ...floatingStyles,
+            }}
+          >
+            {children}
+          </div>
+        </FloatingFocusManager>
       )}
     </>
   );
