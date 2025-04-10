6160a149834e4ed4d16851be49d67f9da45e274b
Denner Vidal <denner.vidal@gmail.com>
2025-04-10 11:22:48 -0400

    fix: move `react-icons` to `peerDependency`

--- packages/cortex-core/src/components/common/common.ts
+++ packages/cortex-core/src/components/common/common.ts
@@ -4,7 +4,7 @@ export const overlay = tv({
   base: 'fixed',
   variants: {
     show: {
-      true: 'bg-opacity-50 z-backdrop bg-black h-full w-full left-0 top-0 transition cursor-pointer',
+      true: 'z-backdrop bg-black/50 h-full w-full left-0 top-0 transition cursor-pointer',
       false: 'invisible',
     },
   },
