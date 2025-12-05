8fd656a09621b3bf9ad0cd4b93dee7738cdc8f4a
oabnerwilliam <abnerwilliam026@gmail.com>
2025-12-05 12:30:53 -0300

    feat(ts-4497): substituindo div por botão nas opções de país

--- packages/cortex-react/src/components/PhoneInput/Option.tsx
+++ packages/cortex-react/src/components/PhoneInput/Option.tsx
@@ -1,5 +1,4 @@
 import { ParsedCountry } from 'react-international-phone';
-import { Country } from './context';
 
 export const PhoneInputOption = ({
   country,
@@ -11,16 +10,15 @@ export const PhoneInputOption = ({
   disableClick?: boolean;
 }) => {
   return (
-    <div
+    <button
       className="flex w-full h-[2rem] items-center justify-between p-centi cursor-pointer hover:bg-gray-100"
       onClick={() => {
-        if (!disableClick) {
-          handleSelectCountry(country);
-        }
+        handleSelectCountry(country);
       }}
+      disabled={disableClick}
     >
       <span>{country?.name}</span>
       <span className="text-gray-400 text-sm">+{country?.dialCode}</span>
-    </div>
+    </button>
   );
 };
