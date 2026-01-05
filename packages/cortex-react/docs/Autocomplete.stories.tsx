4a56cd057845c10b1a6098ef2ff0ce07ae0248b0
oabnerwilliam <abnerwilliam026@gmail.com>
2026-01-05 15:14:34 -0300

    feat(autocomplete): exportando o componentre autocomplete + stories

---
+++ packages/cortex-react/docs/Autocomplete.stories.tsx
@@ -0,0 +1,30 @@
+import { Meta, StoryObj } from '@storybook/react';
+import React, { useState } from 'react';
+import { Autocomplete } from '../src/components/Autocomplete/Autocomplete';
+
+export default {
+  title: 'Cortex/Autocomplete',
+  component: Autocomplete,
+} as Meta<typeof Autocomplete>;
+
+export const Default: StoryObj<typeof Autocomplete> = {
+  render: () => {
+    const [value, setValue] = useState('');
+
+    const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      const value = event.target.value;
+
+      setValue(value);
+    };
+
+    return (
+      <div className="flex items-center h-[200px] w-[400px]">
+        <Autocomplete
+          inputValue={value}
+          setInputValue={setValue}
+          onChange={onChange}
+        />
+      </div>
+    );
+  },
+};
