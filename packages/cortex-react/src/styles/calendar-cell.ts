2ceb079cffc22ff1a2c96ae4f9160ead4412bcb0
oabnerwilliam <abnerwilliam026@gmail.com>
2026-01-20 14:39:55 -0300

    chore: trocando a cor de textos de white para light

--- packages/cortex-react/src/styles/calendar-cell.ts
+++ packages/cortex-react/src/styles/calendar-cell.ts
@@ -13,17 +13,17 @@ export const calendarCell = tv({
     },
     isSelected: {
       true: {
-        cell: 'bg-primary-medium border-2 border-primary-medium text-white hover:bg-primary-medium',
+        cell: 'bg-primary-medium border-2 border-primary-medium text-light hover:bg-primary-medium',
       },
     },
     isSelectionStart: {
       true: {
-        cell: 'bg-primary-medium border-2 border-primary-medium rounded-r-none text-white',
+        cell: 'bg-primary-medium border-2 border-primary-medium rounded-r-none text-light',
       },
     },
     isSelectionEnd: {
       true: {
-        cell: 'bg-primary-medium border-2 border-primary-medium rounded-l-none text-white',
+        cell: 'bg-primary-medium border-2 border-primary-medium rounded-l-none text-light',
       },
     },
     inRange: {
