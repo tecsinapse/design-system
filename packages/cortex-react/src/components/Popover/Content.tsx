64a9ab3d917c4270c2014cba8bc9095f47ef8a70
oabnerwilliam <abnerwilliam026@gmail.com>
2026-01-26 17:08:07 -0300

    feat: trocando cor da borda do popover content

--- packages/cortex-react/src/components/Popover/Content.tsx
+++ packages/cortex-react/src/components/Popover/Content.tsx
@@ -43,7 +43,7 @@ export const PopoverContent = ({
             ref={refs.setFloating}
             {...getFloatingProps()}
             className={clsx(
-              'border border-content-minimal bg-surface-inverse p-[0px] rounded-md shadow-default z-popover',
+              'border border-gray-200 bg-surface-inverse p-0 rounded-md shadow-default z-popover',
               className
             )}
             style={{
