a427b6084d969a1097095f5b0e963a7b807be79e
oabnerwilliam <abnerwilliam026@gmail.com>
2026-02-12 17:09:30 -0300

    feat(dark mode): corrigindo a cor do texto nos modais

--- packages/cortex-core/src/components/modal/modal.ts
+++ packages/cortex-core/src/components/modal/modal.ts
@@ -1,7 +1,7 @@
 import { tv } from 'tailwind-variants';
 
 export const modal = tv({
-  base: 'fixed rounded-micro p-kilo bg-surface-overlay shadow-default flex transition inset-0 z-modal',
+  base: 'fixed rounded-micro p-kilo bg-surface-overlay shadow-default flex transition inset-0 z-modal text-default',
   variants: {
     open: {
       true: 'scale-100 visible',
